c6ee927c0319a70cc79742291ba1186600461069
LeonardoSilva <apx88.0@gmail.com>
2020-06-05 20:33:06 -0400

    fetched localhost data

--- punk-api-react/src/App.js
+++ punk-api-react/src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
+import { Route, Switch } from "react-router-dom";
 
-function App() {
-  return <div className="App">
-    <Button variant="primary">Primary</Button>
-  </div>;
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data: [],
+    };
+  }
+  componentDidMount() {
+    let url = "http://localhost:8080/beers";
+    fetch(url)
+      .then((unparsedData) => unparsedData.json())
+      .then((parsedData) => {
+        this.setState({ data: parsedData });
+      });
+  }
+
+  render() {
+    console.log(this.state.data);
+    return (
+      <div className="App">
+        <Button variant="primary">Primary</Button>
+      </div>
+    );
+  }
 }
 
 export default App;
